2a7f5d9a6107ea9ab00b7930fd3cb879d2440b5f
johnkasonda <john.kasonda@gmail.com>
2020-01-17 15:11:04 +0200

    Feature: Added Models

---
+++ Models/CountryModels.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Models
+{
+    public class CountryModels
+    {
+        public string Name { get; set; }
+        public string ISO2 { get; set; }
+        public string ISO3 { get; set; }
+    }
+}
