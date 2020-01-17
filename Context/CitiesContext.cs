6b70d4f2f8e92dd53f0556d79ac5d67238ddb5fd
johnkasonda <john.kasonda@gmail.com>
2020-01-17 16:38:34 +0200

    Fix: Added Entities and context

---
+++ Context/CitiesContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Entities;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Context
+{
+   
+    public class CitiesContext : DbContext
+    {
+        public DbSet<CountryEntity> Countries { get; set; }
+        public DbSet<CityEntity> Cities { get; set; }
+    }
+}
